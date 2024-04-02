eac221fdae10e40231ed48b2f78d8e5e59dfdced
contug <66022609+contug@users.noreply.github.com>
2024-04-02 15:17:05 +0200

    first commit

---
+++ src/main/java/com/example/triphippiespring/TriphippieSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.triphippiespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriphippieSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriphippieSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/triphippiespring/TriphippieSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triphippiespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriphippieSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
